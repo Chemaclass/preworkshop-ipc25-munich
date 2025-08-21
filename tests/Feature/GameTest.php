1f77d7245bd6f87869b96f7f11848f84ca2794db
Chemaclass <chemaclass@outlook.es>
2025-08-21 17:26:21 +0200

    test_nothing

---
+++ tests/Feature/GameTest.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace KataTest\Feature;
+
+use PHPUnit\Framework\TestCase;
+
+final  class GameTest extends TestCase
+{
+    public function test_nothing(): void
+    {
+        self::assertTrue(true);
+    }
+}
